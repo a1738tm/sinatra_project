7a17427b58e6534e3a52f54305bc1723e0757436
murakami <a1738tm@aiit.ac.jp>
2017-07-16 00:52:16 +0900

    Added post merhod

--- hello.rb
+++ hello.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+require 'csv'
+
+configure do
+  data = {}
+  csv = CSV.read('db/data.csv')
+  csv.each do |row|
+    data[row[0]] = {
+      'capital' => row[1],
+    }
+  end
+  set :data, data
+end
 
 get '/' do
   erb :index
+end
+
+post '/new' do
+  country = params['body']
+  erb :result, :locals => { :country => country, :capital => settings.data[country]['capital'] }
 end
